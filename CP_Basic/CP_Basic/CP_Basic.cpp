705f6a28461b4df91e7f228fa9c0ac8af5f64c00
fish2203 <fish2203@gmail.com>
2023-11-20 17:52:45 +0900

    11.20 수업내용
    수업내용

--- CP_Basic/CP_Basic/CP_Basic.cpp
+++ CP_Basic/CP_Basic/CP_Basic.cpp
@@ -95,6 +95,52 @@ int main()
 	Inc *= 3;
 	Inc /= 3;
 	Inc %= 3;
+
+	cout << "=============================" << endl;
+
+	/*
+		비교 연산
+		크기비교의 결과는 참/거짓으로 나온다.
+		실제 출력에서는 1/0으로 나온다.
+
+		true = 참 = 1
+		false = 거짓 = 0
+	*/
+
+	int Compare1 = 10;
+	int Compare2 = 20;
+
+	// >: 크다, <: 작다 => 방향 고정
+	cout << (Compare1 > Compare2) << endl;
+	cout << (Compare1 < Compare2) << endl;
+	cout << (Compare1 >= 10 ) << endl;
+	cout << (10 >= Compare1) << endl;
+	cout << (Compare1 == Compare2) << endl;
+	cout << (Compare1 != Compare2) << endl;
+
+	//논리연산 : bool 데이터 연산
+	/*
+		AND : && (&: ampersand)
+
+		true && true : true
+		true && false : false
+		=> bool 데이터가 모두 true일때 결과가 true
+
+		OR : ||  (|: pipe)
+
+		true || true : true
+		true || false : true
+		false || false : false
+		=> bool 데이터중 하나라도 true이면 결과가 true
+
+		NOT : !
+
+		!true : false
+		!false : true
+		=>bool 데이터의 결과값을 반대로 부정
+	*/
+	cout << ((Compare1 < Compare2) && (Compare1 < 50)) << endl;
+	cout << !(Compare1 > Compare2) << endl;
 }
 
 // 비트와 바이트에 대한 이해
