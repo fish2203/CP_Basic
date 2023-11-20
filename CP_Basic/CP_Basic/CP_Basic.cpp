d8c04adf85827db11027665f2a818e76b268bcb3
fish2203 <fish2203@gmail.com>
2023-11-20 17:15:23 +0900

    CP Basic Init
    Init

---
+++ CP_Basic/CP_Basic/CP_Basic.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	// 키워드, 예약어: 선언문, 명령문
+	// 키워드: 변수 타입 선언
+
+	int Number1; // 변수 선언
+	Number1 = 10; // 초기값 대입
+
+	int Number2 = 20; // 변수 선언 및 초기화
+
+	int Number3 = 30, Number4 = 40;
+
+	//상수(Constant)
+	const int Number5 = 50;
+
+
+}
